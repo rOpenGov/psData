6be5f3e2bb5d49fe7d4956e3c613406440f67cfc
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-10-16 11:53:53 -0400

    wrote tests and docs for list-of-classes

--- R/list-of-classes.R
+++ R/list-of-classes.R
@@ -1,27 +1,54 @@
-setClass("ListOfClasses", "list",
+##' List of classes
+##'
+##' An S4 subclass of \code{list} which requires all elements of the
+##' list to be the same class.
+##' 
+##' @section Slots:
+##'
+##' \describe{
+##' \item{\code{.Data}}{Object as \code{list}.}
+##' \item{\code{classtype}}{Object as \code{character}. Required classtype for
+##' all elements in the list.}
+##' }
+##'
+##' @section Extends:
+##'
+##' \describe{
+##' \item{\code{list}}{Directly.}
+##' }
+##'
+##' @export
+setClass("ListOfClasses",
+         contains="list",
          representation(classtype="character"))
 
+##' @export
 setValidity("ListOfClasses",
             function(object) {
-                if (length(classtype) > 1) {
+                if (length(object@classtype) > 1) {
                     return("object@classtype has a length > 1")
                 }
-                if (!all(object, is, class2=object@classtype)) {
+                if (!all(sapply(object, is, class2=object@classtype))) {
                     return(sprintf("Not all elements have class %s",
                                    object@classtype))
                 }
                 TRUE
             })
 
-setMethod("c", "ListOfClasses",
-          function(x, ...) {
-              new("ListOfClasses", callNextMethod(x, ...),
+##' @export
+setMethod("c", signature="ListOfClasses",
+          def=function(x, ...) {
+              new("ListOfClasses",
+                  c(x@.Data, ...),
                   classtype=x@classtype)
           })
 
+
+##' @export
 subclass_list_of_classes <- function(class, classtype="ANY",
                                      where=topenv(parent.frame())) {
-    setClass(class, contains="ListOfClasses",
+    setClass(class,
+             contains="ListOfClasses",
              prototype=prototype(list(), classtype=classtype),
              where=where)
 
@@ -30,7 +57,7 @@ subclass_list_of_classes <- function(class, classtype="ANY",
                     if (object@classtype != classtype) {
                         return(sprintf("object@classtype != %s", classtype))
                     }
-                    callNextMethod(object)
+                    validObject(as(object, "ListOfClasses"))
                     TRUE
                 },
                 where=where)
@@ -43,7 +70,6 @@ subclass_list_of_classes <- function(class, classtype="ANY",
     setAs("list", class,
           function(from, to) new(class, from), where=where)
 
-    setMethod("c", class, 
     .f <- function(x) {
         new(class, x)
     }
---
+++ inst/tests/test-list-of-classes.R
@@ -0,0 +1,49 @@
+context("testing list-of-classes")
+
+test_that("test list of classes", {
+    foo <- new("ListOfClasses", 1:4, classtype = "integer")
+    expect_is(foo, "ListOfClasses")
+    expect_equal(foo@.Data, as.list(1:4))
+    expect_equal(foo@classtype, "integer")
+})
+
+test_that("Error if bad classtype", {
+    expect_error(new("ListOfClasses", letters, classtype="integer"),
+                 "Not all elements have class")
+})
+
+test_that("Error if length classtype > 1", {
+    expect_error(new("ListOfClasses", 1:4, classtype=c("integer", "character")),
+                 "length > 1")
+})
+
+test_that("c,ListOfClasses-method works", {
+    foo <- new("ListOfClasses", 1:4, classtype="integer")
+    bar <- new("ListOfClasses", 5:10, classtype="integer")
+    baz <- c(foo, bar)
+    expect_is(baz, "ListOfClasses")
+    expect_equal(baz@.Data, as.list(1:10))
+    expect_equal(baz@classtype, "integer")
+})
+
+test_that("subclass_list_of_classes creates a subclass", {
+    foo <- subclass_list_of_classes("foo", "integer")
+    expect_is(foo, "function")
+    expect_true(isClass("foo"))
+})
+
+test_that("subclass_list_of_classes returned function works", {
+    foo <- subclass_list_of_classes("foo", "integer")
+    bar <- foo(1:10)
+    expect_equal(bar@.Data, as.list(1:10))
+    expect_equal(bar@classtype, "integer")
+})
+
+test_that("subclass_list_of_classes new class gives error if classtype altered", {
+    foo <- subclass_list_of_classes("foo", "integer")
+    bar <- foo(1:10)
+    slot(bar, "classtype") <- "character"
+    bar <- setDataPart(bar, letters)
+    expect_error(validObject(bar), "object@classtype !=")
+})
+
