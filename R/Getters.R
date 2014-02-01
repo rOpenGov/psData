41880c8b0056deca890dbf41e0634162a3f5e3e7
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-01-31 14:45:23 -1100

    duplicates standardised names options

--- R/Getters.R
+++ R/Getters.R
@@ -5,7 +5,10 @@
 #' @param url character string. The URL for the Polity IV data set you would like to download. Note: it must be for the SPSS version of the file.
 #' @param vars character vector containing the variables to keep. If \code{vars = NULL} then the entire data set is returned. Note that the \code{country} and \code{year} variables are always returned.  
 #' @param OutCountryID character string. The type of country ID you would like to include in the output file along with the country name. See \code{\link{countrycode}} for available options. 
+#' @param standardCountryName logical. Whether or not to standardise the country names variable based on \code{country.name} from  \code{\link{countrycode}}.
 #' @param na.rm logical. Drop observations where \code{OutCountryID} is \code{NA}.
+#' @param duplicates character specifying how to handle duplicated country-year observations. Can be set to \code{none} to do nothing, \code{message} to simply report duplicates, \code{drop} to report and drop duplicates, and \code{return} to return a data frame with only duplicated observations (see also \code{fromLast}).
+#' @param fromLast logical indicating if duplication should be considered from the reverse side. Only relevant if \code{duplicates = 'drop'} or \code{duplicates = 'out'}.
 #'   
 #'    
 #' @examples
@@ -22,7 +25,7 @@
 #'    
 #' @export
 
-PolityGet <- function(url = 'http://www.systemicpeace.org/inscr/p4v2012.sav', vars = NULL, OutCountryID = 'iso2c', na.rm = TRUE){
+PolityGet <- function(url = 'http://www.systemicpeace.org/inscr/p4v2012.sav', vars = NULL, OutCountryID = 'iso2c', standardCountryName = TRUE, na.rm = TRUE, duplicates = 'message', fromLast = FALSE){
     # Download underlying Polity IV data 
     tmpfile <- tempfile()
     download.file(url, tmpfile)
@@ -42,8 +45,9 @@ PolityGet <- function(url = 'http://www.systemicpeace.org/inscr/p4v2012.sav', va
     } 
         
     # Include new country ID variable and standardise country names
-    PolityData <- CountryID(data = PolityData, 
-                    OutCountryID = OutCountryID)
+    PolityData <- CountryID(data = PolityData, OutCountryID = OutCountryID,
+                    timeVar = 'year', duplicates = duplicates, 
+                    standardCountryName = standardCountryName, fromLast = fromLast)
   # Drop NAs for OutCountryID
     if (isTRUE(na.rm)){
         PolityData <- DropNA.psData(data = PolityData, 
@@ -60,7 +64,10 @@ PolityGet <- function(url = 'http://www.systemicpeace.org/inscr/p4v2012.sav', va
 #' @param url character string. The URL for the Polity IV data set you would like to download. Note: the link must be to a Stata formated file.
 #' @param vars character vector containing the variables to keep. If \code{vars = NULL} then the entire data set is returned. Note that \code{country} and \code{year} variables are always returned.  
 #' @param OutCountryID character string. The type of country ID you would like to include in the output file along with the country name. See \code{\link{countrycode}} for available options. 
+#' @param standardCountryName logical. Whether or not to standardise the country names variable based on \code{country.name} from  \code{\link{countrycode}}.
 #' @param na.rm logical. Drop observations where \code{OutCountryID} is \code{NA}.
+#' @param duplicates character specifying how to handle duplicated country-year observations. Can be set to \code{none} to do nothing, \code{message} to simply report duplicates, \code{drop} to report and drop duplicates, and \code{return} to return a data frame with only duplicated observations (see also \code{fromLast}).
+#' @param fromLast logical indicating if duplication should be considered from the reverse side. Only relevant if \code{duplicates = 'drop'} or \code{duplicates = 'out'}.
 #'   
 #'    
 #' @examples
@@ -77,7 +84,7 @@ PolityGet <- function(url = 'http://www.systemicpeace.org/inscr/p4v2012.sav', va
 #'    
 #' @export
 
-DpiGet <- function(url = 'http://siteresources.worldbank.org/INTRES/Resources/469232-1107449512766/DPI2012.dta', vars = NULL, OutCountryID = 'iso2c', na.rm = TRUE){
+DpiGet <- function(url = 'http://siteresources.worldbank.org/INTRES/Resources/469232-1107449512766/DPI2012.dta', vars = NULL, OutCountryID = 'iso2c', standardCountryName = TRUE, na.rm = TRUE, duplicates = 'message', fromLast = FALSE){
     # Download underlying Dpi IV data 
     tmpfile <- tempfile()
     download.file(url, tmpfile)
@@ -93,10 +100,11 @@ DpiGet <- function(url = 'http://siteresources.worldbank.org/INTRES/Resources/46
         DpiData <- DpiData[, Vars] 
     } 
         
-    # Include new country ID variable and standardise country names
+    # Include new country ID variable
     DpiData <- CountryID(data = DpiData, 
-                    OutCountryID = OutCountryID,
-                    countryVar = 'countryname')
+                    OutCountryID = OutCountryID, countryVar = 'countryname',
+                    timeVar = 'year', duplicates = duplicates, 
+                    standardCountryName = standardCountryName,fromLast = fromLast)
 
     # Drop NAs for OutCountryID
     if (isTRUE(na.rm)){
--- R/utils.R
+++ R/utils.R
@@ -1,26 +1,81 @@
-#' Internal function for creating standardised country names and ID variables
+#' Function for creating standardised country names and ID variables
 #'
+#' Function for creating standardised country names and ID variables based on capabilities from the \code{\link{countrycode}} package. The function also reports if duplicated country IDs have been created and lets the user either drop these or return only duplicated values for inspection.
 #' @param data a data frame object
 #' @param countryVar character string naming the country.name variable. See \code{\link{countrycode}}.
 #' @param OutCountryID character string. The type of country ID you would like to include in the output file along with the country name. See \code{\link{countrycode}} for available options. 
+#' @param standardCountryName logical. Whether or not to standardise the country names variable based on \code{country.name} from  \code{\link{countrycode}}.
+#' @param duplicates character specifying how to handle duplicated country or country-time observations (for the latter see \code{timeVar}). Can be set to \code{none} to do nothing, \code{message} to simply report duplicates, \code{drop} to report and drop duplicates, and \code{return} to return a data frame with only duplicated observations (see also \code{fromLast}).
+#' @param timeVar character string indicating the name of a time variable. For example, country time series often have separate rows based on a \code{year} variable. This is used solely to determine if there are duplicated country-time values.
+#' @param fromLast logical indicating if duplication should be considered from the reverse side. Only relevant if \code{duplicates = 'drop'} or \code{duplicates = 'out'}.
+#'
+#' @seealso {\code{\link{duplicated}}}
 #'
 #' @import countrycode
-#' @keywords internals
 #' @export
 
-CountryID <- function(data, countryVar = 'country', OutCountryID = 'iso2c'){
+CountryID <- function(data, countryVar = 'country', OutCountryID = 'iso2c', standardCountryName = TRUE, duplicates = 'message', timeVar = NULL, fromLast = FALSE){
+  # Copy data set for duplicates reporting
+  OriginalData <- data
+
+  # duplicates argument error message
+  dupValues <- c('none', 'message', 'drop', 'return')
+  if (!isTRUE(duplicates %in% dupValues)){
+    stop('duplicates must be either "none", "message", "drop", or "return".')
+  }
+
   # Include new country ID variable
   data[, OutCountryID] <- countrycode(data[, countryVar], 
                             origin = 'country.name',
                             destination = OutCountryID)
 
   # Standardise country names
+  if (isTRUE(standardCountryName)){
   data <- MoveFront(data, countryVar)
   data <- data[, -1]
   data$country <- countrycode(data[, OutCountryID], 
                             origin = OutCountryID,
                             destination = 'country.name') 
   data <- MoveFront(data, c(OutCountryID, 'country'))
+  }
+  else if (!isTRUE(standardCountryName)){
+    data <- MoveFront(data = data, OutCountryID)
+  }
+
+  # Inspect duplicated values
+  if (duplicates != 'none'){
+    if (is.null(timeVar)){
+      Var1 <- countryVar
+      Var2 <- OutCountryID
+    }
+    else if (!is.null(timeVar)){
+      Var1 <- c(countryVar, timeVar)
+      Var2 <- c(OutCountryID, timeVar)
+    }
+    # Find original number of duplicated observations
+    OriginDupDF <- OriginalData[duplicated(OriginalData[, Var1]), ]
+    OriginCount <- nrow(OriginDupDF)
+
+    # Find duplicated observations in output data set
+    TransDupDF <- data[duplicated(data[, Var2]), ]
+    TransCount <- nrow(TransDupDF)
+
+    # Difference between the original and transformed data sets
+    DifDups <- TransCount - OriginCount
+
+    # Output
+    if (duplicates == 'message'){
+      message(paste(DifDups, 'duplicated values were created when standardising the country ID with', OutCountryID, '. \n To inspect duplicated rows set duplicates = "out".\n'))
+    }
+    else if (duplicates == 'drop'){
+      data <- data[!duplicated(data[, Var2], fromLast = fromLast), ]
+      message(paste(DifDups, 'duplicated rows were dropped.\n'))
+    }
+    else if (duplicates == 'return'){
+      data <- data[duplicated(data[, Var2], fromLast = fromLast), ]
+      message(paste('Only duplicated rows returned.\n'))
+    }
+  }
   return(data)
 }
 
@@ -58,6 +113,6 @@ DropNA.psData <- function(data, Var)
           TotalDropped <- sum(RowNA)
   }        
 
-  message(paste0(TotalDropped, " observations dropped from the data frame based on missing values of the standardised ID variable\n\n"))
+  message(paste0(TotalDropped, " observations dropped based on missing values of the standardised ID variable.\n\n"))
   return(DataNoNA)
 }
