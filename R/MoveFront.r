728569ced71260bb60faa860d509eaf47a51b20b
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-03-05 09:41:16 +0100

    roxygen/file name bug fix

--- R/MoveFront.r
+++ R/MoveFront.r
@@ -8,37 +8,10 @@
 #' @param ignore.case logical. If \code{FALSE}, the variable name matching is case sensitive and if \code{TRUE}, case is ignored during matching. Only available when \code{exact = FALSE}.
 #' @param fixed logical. If \code{TRUE}, pattern is a string to be matched as is. Overrides all conflicting arguments. Only available when \code{exact = FALSE}.
 #'
-#' @examples 
-#' # Create fake data
-#' A <- B <- C <- 1:50
-#' OldOrder <- data.frame(A, B, C)
-#' 
-#' # Move C to front
-#' NewOrder1 <- MoveFront(OldOrder, "C")
-#' names(NewOrder1)
-#'
-#' # Move B and A to the front
-#' NewOrder2 <- MoveFront(OldOrder, c("B", "A"))
-#' names(NewOrder2)
-#'
-#' ## Non-exact matching (example from Felix Hass)
-#'  # Create fake data 
-#'  df <- data.frame(dummy = c(1,0), Name = c("Angola", "Chad"),
-#'                  DyadName = c("Government of Angola - UNITA", 
-#'                  "Government of Chad - FNT"),
-#'                  Year = c("2002", "1992"))
-#'                  
-#'  df <- MoveFront(df, c("Name", "Year"), exact = FALSE)
-#'  
-#'  names(df)
-#'  
-#'  df <- MoveFront(df, c("Name", "Year"), exact = TRUE)
-#'
-#'  names(df)
 #'  
 #' @source Based primarily on a Stack Overflow answer written by rcs: \url{http://stackoverflow.com/questions/3369959/moving-columns-within-a-data-frame-without-retyping}. 
-#'
-#' @export
+#' @keywords internals
+#' @noRd
 
 MoveFront <- function(data, Var, exact = TRUE, ignore.case = NULL, fixed = NULL)
 {
